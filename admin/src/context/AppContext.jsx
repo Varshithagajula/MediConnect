54f58ecd178a18b3936ecf462cf08f8006aa24d8
Varshithagajula <varshithagajula829@gmail.com>
2025-05-16 12:41:30 +0530

    Update AppContext.jsx

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -5,8 +5,8 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-    const currency = import.meta.env.VITE_CURRENCY
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const currency = '₹'
+    const backendUrl = "https://mediconnect-backend-lq1k.onrender.com"
 
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
